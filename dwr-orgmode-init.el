df3792ad364aca65d03da6de7c3eb0ee5e2af880
Don Ross <donald.ross@oracle.com>
2018-10-03 10:28:46 -0400

    Update of org-mode config

--- dwr-orgmode-init.el
+++ dwr-orgmode-init.el
@@ -12,12 +12,45 @@
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-cb" 'org-switchb)
 
-(defun my-org-mode-config ()
-  "For use in `org-mode-hook'."
-  (local-set-key (kbd "S-<right>") 'org-todo)
-  (setq org-todo-keywords '((sequence "TODO" "WORKING" "WAITING" "|" "DONE" "DELEGATED")))
-  
-)
-;; add to hook
-(add-hook 'org-mode-hook 'my-org-mode-config)
+(setq org-agenda-files '("~/gtd/inbox.org"
+                         "~/gtd/gtd.org"
+                         "~/gtd/tickler.org"))
+(setq org-todo-keywords '((sequence "TODO" "WORKING" "WAITING" "|" "DONE" "DELEGATED")))
+(setq org-capture-templates '(("t" "Todo [inbox]" entry
+			       (file+headline "~/gtd/inbox.org" "Tasks")
+			       "* TODO %i%?")
+			      ("T" "Tickler" entry
+			       (file+headline "~/gtd/tickler.org" "Tickler")
+			       "* %i%? \n %U")))
+(setq org-outline-path-complete-in-steps nil)
+(setq org-refile-targets '(("~/gtd/gtd.org" :maxlevel . 3)
+			   ("~/gtd/someday.org" :maxlevel . 1)
+			   ("~/gtd/tickler.org" :maxlevel . 2)))
+(setq org-refile-allow-creating-parent-nodes 'confirm)
+
+(setq org-agenda-custom-commands
+      '(("o" "At the office" tags-todo "office"
+         ((org-agenda-overriding-header "Office")))
+
+
+	("n" "All Next Tasks" todo ""
+         ((org-agenda-overriding-header "Next")
+          (org-agenda-skip-function #'my-org-agenda-skip-all-siblings-but-first)))))
+
+(defun my-org-agenda-skip-all-siblings-but-first ()
+  "Skip all but the first non-done entry."
+  (let (should-skip-entry)
+    (unless (org-current-is-todo)
+      (setq should-skip-entry t))
+    (save-excursion
+      (while (and (not should-skip-entry) (org-goto-sibling t))
+        (when (org-current-is-todo)
+          (setq should-skip-entry t))))
+    (when should-skip-entry
+      (or (outline-next-heading)
+          (goto-char (point-max))))))
+
+(defun org-current-is-todo ()
+  (or (string= "TODO" (org-get-todo-state))
+      (string= "WORKING" (org-get-todo-state))))
 
