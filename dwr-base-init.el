f069af7a5e69eb99e032d08b1b5ac33f692dd229
Don Ross <donald.ross@oracle.com>
2019-05-17 11:16:20 -0400

    updates

--- dwr-base-init.el
+++ dwr-base-init.el
@@ -16,6 +16,23 @@
 (global-set-key (kbd "M-]") 'ace-window)
 (global-set-key (kbd "C-M-]") 'ace-swap-window)
 
+;; Ace Jump Mode
+(autoload
+  'ace-jump-mode
+  "ace-jump-mode"
+  "Emacs quick move minor mode"
+  t)
+(define-key global-map (kbd "M-[") 'ace-jump-mode)
+
+(autoload
+  'ace-jump-mode-pop-mark
+  "ace-jump-mode"
+  "Ace jump back:-)"
+  t)
+(eval-after-load "ace-jump-mode"
+  '(ace-jump-mode-enable-mark-sync))
+(define-key global-map (kbd "C-x SPC") 'ace-jump-mode-pop-mark)
+
 ;; Setup basic Helm with M-x override
 (require 'helm)
 ;;(helm-mode 1)
